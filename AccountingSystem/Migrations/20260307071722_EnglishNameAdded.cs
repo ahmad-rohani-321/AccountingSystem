0524cbb0254e8cdfe216121b0d3e602f500d261b
Ahmad Rohani <91419734+ahmad-rohani-321@users.noreply.github.com>
2026-03-07 11:49:56 +0430

    NewCommit

---
+++ AccountingSystem/Migrations/20260307071722_EnglishNameAdded.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AccountingSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class EnglishNameAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EnglishName",
+                table: "Categories",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: "f5b9b7e7-2d3a-4b4d-a1b5-1b3f2a7a9e01",
+                column: "PasswordHash",
+                value: "AQAAAAIAAYagAAAAECc3WPBV1KuvL7MlEByAkt1W1DXSnITNFMxhDMUBRK2HggjnOGGgo69JhBQQEvukcw==");
+
+            migrationBuilder.UpdateData(
+                table: "WareHouses",
+                keyColumn: "ID",
+                keyValue: 1,
+                column: "CreationDate",
+                value: new DateTime(2026, 3, 7, 11, 47, 21, 939, DateTimeKind.Local).AddTicks(4140));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EnglishName",
+                table: "Categories");
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: "f5b9b7e7-2d3a-4b4d-a1b5-1b3f2a7a9e01",
+                column: "PasswordHash",
+                value: "AQAAAAIAAYagAAAAELOGuYc9glXeeQnOZDJjZpPL1Xn/oLT2JQtc9nPEhjQqZDbwq12QHtS3k3i8TJxung==");
+
+            migrationBuilder.UpdateData(
+                table: "WareHouses",
+                keyColumn: "ID",
+                keyValue: 1,
+                column: "CreationDate",
+                value: new DateTime(2026, 3, 7, 11, 22, 58, 765, DateTimeKind.Local).AddTicks(8782));
+        }
+    }
+}
